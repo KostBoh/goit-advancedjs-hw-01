bb290f483ed5b4889d24b7365b91fc16ba1abdac
KostBoh <bohdan.kostya@gmail.com>
2024-01-15 00:36:07 +0200

    add change task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import { galleryItems } from './gallery-items';
+
+import SimpleLightbox from 'simplelightbox';
+
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+const createMarkup = galleryItems
+  .map(
+    ({ original, description, preview }) => `
+      <li>
+        <a href="${original}" data-lightbox="gallery" data-title="${description}">
+          <img src="${preview}" alt="${description}" />
+        </a>
+      </li>`
+  )
+  .join('');
+galleryContainer.insertAdjacentHTML('beforeend', createMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'title',
+  captionDelay: 250,
+});
