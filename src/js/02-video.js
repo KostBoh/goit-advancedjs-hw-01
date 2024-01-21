f890713c9e30125a96e00684426a4c827592f4a6
KostBoh <bohdan.kostya@gmail.com>
2024-01-22 00:12:56 +0200

    add change vimeo

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+document.addEventListener('DOMContentLoaded', function () {
+  const player = new Vimeo('vimeo-player');
+  const storageKey = 'videoplayer-current-time';
+  let playbackStarted = false;
+
+  const saveCurentTime = throttle(function () {
+    player.getCurrentTime().then(function (currentTime) {
+      localStorage.setItem(storageKey, currentTime);
+    });
+  }, 1000);
+
+  player.on('timeupdate', saveCurentTime);
+
+  const savedTime = localStorage.getItem(storageKey);
+
+  if (savedTime) {
+    player.setCurrentTime(savedTime);
+  }
+
+  const playButton = document.getElementById('playButton');
+  if (playButton) {
+    playButton.addEventListener('click', function () {
+      if (!playbackStarted) {
+        player.play();
+        playbackStarted = true;
+      }
+    });
+  }
+});
